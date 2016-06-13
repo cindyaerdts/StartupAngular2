a91506932734e53b5b46dd3ae163fd5959c57921
Cindy <caerdts@outlook.com>
2016-06-13 22:35:13 +0200

    AngularJS2.0 startup

---
+++ app/app.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'hello-user',
+    template: `
+                <h1>Hello everybody!</h1>
+                <h2>Dit is Angular 2</h2>
+                `
+})
+
+export /**
+ * AppComponent
+ */
+class AppComponent {
+    // constructor(parameters) {
+        
+    // }
+}
---
+++ app/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// import dependencies (don't forget to import your component!)
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var app_component_1 = require("./app.component");
+// bootstrap our app
+platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent);
+//# sourceMappingURL=main.js.map
